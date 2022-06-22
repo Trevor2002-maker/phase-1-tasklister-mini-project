8b3b0f53478f20e1e7dae6b5cd21a55c9264b5e9
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2022-06-22 13:49:25 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const newForm = document.getElementById("create-task-form");
+  const newDescription = document.getElementById("new-task-description");
+  const newPriority = document.getElementById("new-task-priority");
+
+
+  const newUl = document.getElementById("tasks");
+
+  
+  newForm.addEventListener("submit", createTask);
 });
+
+const createTask = event => {
+  event.preventDefault();
+  
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  appendTask(newTask);
+  event.target.reset();
+};
+
+const appendTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
